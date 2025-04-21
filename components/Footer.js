b96217b17e406d4d23b63d57115112e8f8ec0421
PumpaCto <pumpa6827@gmail.com>
2025-04-21 15:59:10 +0300

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-black border-t border-orange-500 py-6 text-center text-sm text-gray-400">
+      <p>© {new Date().getFullYear()} Pumpa Token. All rights reserved.</p>
+    </footer>
+  );
+}
