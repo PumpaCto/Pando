e30f3cd9bf5d7f2a3accc9cd4ffd410ccf480555
PumpaCto <pumpa6827@gmail.com>
2025-04-21 17:07:35 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
-import '@/styles/globals.css';
-import '@/styles/animations.css';
+import '../styles/globals.css';
+import '../styles/animations.css';
 
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import ScrollToTop from '@/components/ScrollToTop';
-import StickyBuy from '@/components/StickyBuy';
-import Preloader from '@/components/Preloader';
-import AlertModal from '@/components/AlertModal';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import ScrollToTop from '../components/ScrollToTop';
+import StickyBuy from '../components/StickyBuy';
+import Preloader from '../components/Preloader';
+import AlertModal from '../components/AlertModal';
 
 export default function App({ Component, pageProps }) {
   return (
